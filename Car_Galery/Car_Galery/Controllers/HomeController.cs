77a29524a22e2e71389c3c583f74ecc5f2aedf92
huzeyfe ulutaş <huzeyfe845@hotmail.com>
2020-02-12 13:50:03 +0300

    test

--- Car_Galery/Car_Galery/Controllers/HomeController.cs
+++ Car_Galery/Car_Galery/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Car_Galery.Entities;
+using Car_Galery.Models;
 
 namespace Car_Galery.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(ExampleViewModel ba)
         {
-            return View();
+
+                List<Vehicle> c = new List<Vehicle>();
+
+                c = ba.Vehicles;
+
+                string x = ba.SelectedVAlue;
+
+
+
+            return View(ba);
+
         }
 
         public ActionResult About()
@@ -18,6 +30,22 @@ namespace Car_Galery.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(ExampleViewModel a)
+        {
+            if (a.SelectedVAlue == "merhaba")
+            {
+                a.Vehicles = new List<Vehicle>();
+                a.Vehicles.Add(new Vehicle()
+                {
+                    Name = "Mercedes"
+                });
+            }
+
+
+            return RedirectToAction("Index", "Home", a);
         }
 
     }
---
+++ Car_Galery/Car_Galery/Models/ExampleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Car_Galery.Entities;
+
+namespace Car_Galery.Models
+{
+    public class ExampleViewModel
+    {
+        public List<Vehicle> Vehicles { get; set; }
+
+        public string SelectedVAlue { get; set; }
+
+
+    }
+}
