3dcde06883c01c31aa993d2fe2cdefc6d80157ca
huzeyfe ulutaş <huzeyfe845@hotmail.com>
2020-02-12 22:51:19 +0300

    Revert "test"
    This reverts commit 77a29524a22e2e71389c3c583f74ecc5f2aedf92.

--- Car_Galery/Car_Galery/Controllers/HomeController.cs
+++ Car_Galery/Car_Galery/Controllers/HomeController.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Car_Galery.Entities;
-using Car_Galery.Models;
 
 namespace Car_Galery.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(ExampleViewModel ba)
+        public ActionResult Index()
         {
-
-                List<Vehicle> c = new List<Vehicle>();
-
-                c = ba.Vehicles;
-
-                string x = ba.SelectedVAlue;
-
-
-
-            return View(ba);
-
+            return View();
         }
 
         public ActionResult About()
@@ -30,22 +18,6 @@ namespace Car_Galery.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult About(ExampleViewModel a)
-        {
-            if (a.SelectedVAlue == "merhaba")
-            {
-                a.Vehicles = new List<Vehicle>();
-                a.Vehicles.Add(new Vehicle()
-                {
-                    Name = "Mercedes"
-                });
-            }
-
-
-            return RedirectToAction("Index", "Home", a);
         }
 
     }
