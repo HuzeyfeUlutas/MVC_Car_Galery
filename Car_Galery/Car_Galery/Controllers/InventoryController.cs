b43c80a9f28d38b39e8c5a83dc4fe7b61c4b2c5c
huzeyfe ulutaş <huzeyfe845@hotmail.com>
2020-02-24 16:48:51 +0300

    VehicleListHelper Add

--- Car_Galery/Car_Galery/Controllers/InventoryController.cs
+++ Car_Galery/Car_Galery/Controllers/InventoryController.cs
@@ -11,6 +11,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Car_Galery.Context;
 using Car_Galery.Entities;
+using Car_Galery.Helpers;
 using Car_Galery.Managers;
 using Car_Galery.Managers.Abstract;
 using Car_Galery.Models;
@@ -78,105 +79,7 @@ namespace Car_Galery.Controllers
 
             InventoryViewModel ıvm = new InventoryViewModel();
 
-            var query = unitOfWork.GetRepository<Vehicle>().GetAll().AsQueryable();
-
-
-            #region Filter
-            if(fm.Filtered == true)
-            {
-                int? ku = fm.TypeId;
-                if (fm.TypeId != 0 && fm.TypeId != null)
-                {
-                    query = query.Where(v => v.TypeId == fm.TypeId);
-                }
-
-                ku = fm.BrandId;
-                if (fm.BrandId != 0 && fm.BrandId != null)
-                {
-                    query = query.Where(v => v.BrandId == fm.BrandId);
-                }
-
-                ku = fm.ModelId;
-                if (fm.ModelId != 0 && fm.ModelId != null)
-                {
-                    query = query.Where(v => v.ModelId == fm.ModelId);
-                }
-
-                if(!String.IsNullOrWhiteSpace(fm.FuelType))
-                {
-                    query = query.Where(v => v.Fuel == fm.FuelType);
-                }
-
-                if(!String.IsNullOrWhiteSpace(fm.Transmission))
-                {
-                    query = query.Where(v => v.Transmission == fm.Transmission);
-                }
-
-                if(!String.IsNullOrWhiteSpace(fm.Year))
-                {
-                    query = query.Where(v => v.Year == fm.Year);
-                }
-
-                if (fm.MinPrice != null)
-                {
-                    query = query.Where(v => v.Price >= fm.MinPrice);
-                }
-
-                if (fm.MaxPrice != null)
-                {
-                    query = query.Where(v => v.Price <= fm.MaxPrice);
-                }
-
-                if (fm.MinKm != null)
-                {
-                    query = query.Where(v => v.Km >= fm.MinKm);
-                }
-
-                if (fm.MaxKm != null)
-                {
-                    query = query.Where(v => v.Km <= fm.MaxKm);
-                }
-
-            }
-            #endregion
-
-            #region Brand Categories
-            else if (fm.BrandId != 0 && fm.BrandId != null)
-            {
-                query = query.Where(v => v.BrandId == fm.BrandId);
-
-                if (fm.ModelId != 0 && fm.ModelId != null)
-                {
-                    query = query.Where(v => v.ModelId == fm.ModelId);
-                }
-            }
-            #endregion
-
-            #region Search Text
-            if(!String.IsNullOrWhiteSpace(fm.SearchText))
-            {
-                query = query.Where(v => v.Name.Contains(fm.SearchText));
-            }
-            #endregion
-
-            #region Sort
-            if (!String.IsNullOrWhiteSpace(fm.SortBy))
-            {
-                switch (fm.SortBy)
-                {
-                    case "Name":
-                        query = query.OrderBy(v => v.Name);
-                        break;
-                    case  "Price":
-                        query = query.OrderBy(v => v.Price);
-                        break;
-                    case "Km":
-                        query = query.OrderBy(v => v.Km);
-                        break;
-
-                }
-            }
-            #endregion
+            var query = VehicleListHelper.Filter(fm, unitOfWork);
 
             List<VehicleModel> vhList = new List<VehicleModel>();
 
---
+++ Car_Galery/Car_Galery/Helpers/VehicleListHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Car_Galery.Models;
+using Car_Galery.Context;
+using Car_Galery.Entities;
+using Car_Galery.Managers.Abstract;
+
+namespace Car_Galery.Helpers
+{
+    public class VehicleListHelper
+    {
+        public static IQueryable Filter(FilterModel fm, IUnitOfWork unitOfWork)
+        {
+
+            var query = unitOfWork.GetRepository<Vehicle>().GetAll().AsQueryable();
+             #region Filter
+            if(fm.Filtered == true)
+            {
+                if (fm.TypeId != 0 && fm.TypeId != null)
+                {
+                    query = query.Where(v => v.TypeId == fm.TypeId);
+                }
+
+
+                if (fm.BrandId != 0 && fm.BrandId != null)
+                {
+                    query = query.Where(v => v.BrandId == fm.BrandId);
+                }
+
+                if (fm.ModelId != 0 && fm.ModelId != null)
+                {
+                    query = query.Where(v => v.ModelId == fm.ModelId);
+                }
+
+                if(!String.IsNullOrWhiteSpace(fm.FuelType))
+                {
+                    query = query.Where(v => v.Fuel == fm.FuelType);
+                }
+
+                if(!String.IsNullOrWhiteSpace(fm.Transmission))
+                {
+                    query = query.Where(v => v.Transmission == fm.Transmission);
+                }
+
+                if(!String.IsNullOrWhiteSpace(fm.Year))
+                {
+                    query = query.Where(v => v.Year == fm.Year);
+                }
+
+                if (fm.MinPrice != null)
+                {
+                    query = query.Where(v => v.Price >= fm.MinPrice);
+                }
+
+                if (fm.MaxPrice != null)
+                {
+                    query = query.Where(v => v.Price <= fm.MaxPrice);
+                }
+
+                if (fm.MinKm != null)
+                {
+                    query = query.Where(v => v.Km >= fm.MinKm);
+                }
+
+                if (fm.MaxKm != null)
+                {
+                    query = query.Where(v => v.Km <= fm.MaxKm);
+                }
+
+            }
+            #endregion
+
+            #region Brand Categories
+            else if (fm.BrandId != 0 && fm.BrandId != null)
+            {
+                query = query.Where(v => v.BrandId == fm.BrandId);
+
+                if (fm.ModelId != 0 && fm.ModelId != null)
+                {
+                    query = query.Where(v => v.ModelId == fm.ModelId);
+                }
+            }
+            #endregion
+
+            #region Search Text
+            if(!String.IsNullOrWhiteSpace(fm.SearchText))
+            {
+                query = query.Where(v => v.Name.Contains(fm.SearchText));
+            }
+            #endregion
+
+            #region Sort
+            if (!String.IsNullOrWhiteSpace(fm.SortBy))
+            {
+                switch (fm.SortBy)
+                {
+                    case "Name":
+                        query = query.OrderBy(v => v.Name);
+                        break;
+                    case  "Price":
+                        query = query.OrderBy(v => v.Price);
+                        break;
+                    case "Km":
+                        query = query.OrderBy(v => v.Km);
+                        break;
+
+                }
+            }
+            #endregion
+
+            return query;
+        }
+    }
+}
